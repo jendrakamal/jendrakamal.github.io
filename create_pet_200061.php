81d9f6a8ef57ff3498bb0d677cfbb58cf10afa0a
jendrakamal <93979742+jendrakamal@users.noreply.github.com>
2021-11-30 18:06:15 +0700

    Delete create_pet_200061.php

--- create_pet_200061.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo $_POST['pet_name_200061']  . "<br>"; 
-echo $_POST['pet_type_200061']  . "<br>";
-echo $_POST['pet_gender_200061']  . "<br>";
-echo $_POST['pet_age_200061']  . "<br>";
-echo $_POST['pet_owner_200061']  . "<br>";
-echo $_POST['pet_addres_200061']  . "<br>";
-echo $_POST['pet_phone_200061']  . "<br>";
-?>
